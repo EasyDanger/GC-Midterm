c5adc347c3174587cf7e9e5d76bbc601fb1acaa9
Michael Kincaid <mbkincaid.1@gmail.com>
2018-10-25 14:58:54 -0400

    Payment 1st

---
+++ src/gcMidterm/Payment.java
@@ -0,0 +1,51 @@
+package gcMidterm;
+
+import java.util.Scanner;
+
+public class Payment {
+	
+	private static boolean getCash(String cash) {
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("How much would you like to pay?($00.00)");
+		String userAmount = scnr.nextLine();
+		
+		if (userAmount.matches("\\$\\d+\\.\\d{2}")) {
+			return true;
+		}
+		return false;
+	
+	}
+
+	private static boolean getCheck(String check) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("What is your check number?");
+		String checkNumber = scnr.nextLine();
+		if (checkNumber.matches("\\d+")) {
+			return true;
+		}
+		return false;
+		
+	}
+	private static boolean getCredit(String credit) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("What is your cc number?");
+		String userCC = scnr.nextLine();
+		System.out.println("Experation?");
+		String userExpiration = scnr.nextLine();
+		System.out.println("CVV?");
+		String userCVV = scnr.nextLine();
+		
+		if (userCC.matches("\\d{4}\\-\\d{4}\\-\\d{4}\\-\\d{4}")) {
+			if (userExpiration.matches("\\d{2}\\/\\d{2}")) {
+				if (userCVV.matches("\\d{3}")) {
+					return true;
+				}
+			}
+		}
+		else return false;
+
+		
+	}
+
+}
